d6f80cad04a17ac3972dc45947abf2ccd1b1ac7b
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-04-11 12:24:42 -0500

    routed everything and tested it on postman.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :users
+
+  # User
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
+  # Sessions
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/hello', to: 'application#hello_world'
   # Defines the root path route ("/")
   # root "articles#index"
 end
